e58bfb437889e07bea1c9592e2216518c9300089
brinkz <isabel.sbg11@gmail.com>
2022-08-13 15:43:43 -0600

    metodo hablar

--- paki.js
+++ paki.js
@@ -4,9 +4,14 @@ class Pakiman{
         this.vida = vida;
         this.ataque = ataque;
     }
+
+    hablar(){
+        alert(this.nombre);
+    }
 }
 
 var cauchin = new Pakiman("Cauchin", 100, 30);
 var pokacho = new Pakiman("Pokacho", 80, 50);
 var tocinauro = new Pakiman("Tocinauro", 120, 40); 
 console.log(cauchin, pokacho, tocinauro);
+cauchin.hablar();
