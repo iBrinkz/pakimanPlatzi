469f0e3c929b66e2a14e4653218f8f196af4c8ab
brinkz <isabel.sbg11@gmail.com>
2022-08-16 16:30:33 -0600

    mostrar 3 pakimanes usando clase y array asociativo

--- paki.js
+++ paki.js
@@ -1,17 +1,35 @@
+var imagenes = [];
+imagenes["Pokacho"] = "pollo.webp";
+imagenes["Cauchin"] = "vaca.png";
+imagenes["Tocinauro"] = "cerdo.png";
+
 class Pakiman{
     constructor(nombre, vida, ataque){
+        this.imagen = new Image();
         this.nombre = nombre;
         this.vida = vida;
         this.ataque = ataque;
+
+        this.imagen.src = imagenes[this.nombre];
     }
 
     hablar(){
-        alert(this.nombre);
+        
+    }
+    mostrar(){
+        document.body.appendChild(this.imagen);
+        document.write("<br /><strong>" + this.nombre + "</strong><br />");
+        document.write("Vida: " + this.vida + "<br />");
+        document.write("Ataque: " + this.ataque + "<hr />");
     }
 }
 
 var cauchin = new Pakiman("Cauchin", 100, 30);
 var pokacho = new Pakiman("Pokacho", 80, 50);
 var tocinauro = new Pakiman("Tocinauro", 120, 40); 
-console.log(cauchin, pokacho, tocinauro);
+
 cauchin.hablar();
+
+cauchin.mostrar();
+tocinauro.mostrar();
+pokacho.mostrar();
