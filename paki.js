efa20ec3edfbf2babffd997c918c7431641cbd92
brinkz <isabel.sbg11@gmail.com>
2022-08-16 17:55:22 -0600

    creando coleccion de pakiman con push

--- paki.js
+++ paki.js
@@ -7,6 +7,10 @@ var cauchin = new Pakiman("Cauchin", 100, 30);
 var pokacho = new Pakiman("Pokacho", 80, 50);
 var tocinauro = new Pakiman("Tocinauro", 120, 40); 
 
-cauchin.mostrar();
-tocinauro.mostrar();
-pokacho.mostrar();
+var coleccion = [];
+
+coleccion.push(cauchin);
+coleccion.push(pokacho);
+coleccion.push(tocinauro);
+
+console.log(coleccion);
